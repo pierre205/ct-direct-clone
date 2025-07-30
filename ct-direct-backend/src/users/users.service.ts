fc346f370da2a01cc15997c654c19585ce1b2d0b
pierre-alexandre cochet <76907126+pierre205@users.noreply.github.com>
2025-07-30 14:18:09 +0200

    init project

---
+++ ct-direct-backend/src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(user);
+  }
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+
+  findByEmail(email: string) {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    await this.usersRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number) {
+    await this.usersRepository.delete(id);
+    return { deleted: true };
+  }
+}
